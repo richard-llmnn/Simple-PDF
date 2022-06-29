35ca884911b47c6f66e9442b80fe7884c22fc974
rullmann <69484873+richard-llmnn@users.noreply.github.com>
2022-06-29 22:08:12 +0200

    add basic structure

---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require( 'path' );
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    entry: {
+        app: "./src/index.js",
+    },
+    mode: "development",
+    output: {
+        path: path.resolve( __dirname, 'dist' ),
+    },
+    devServer: {
+        open: false,
+        port: 9000,
+        hot: true,
+        liveReload: true,
+        static: {
+            directory: path.join(__dirname, 'dist'), // to make icons available because they are static generated
+        },
+        client: {
+            progress: true,
+            overlay: true,
+            logging: "error",
+        },
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin(),
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, "src/app.html"),
+            inject: "head", // if is set to "body", "head" or true you have to remove the webpack tags from the template!
+            favicon: false, // do not user favicon, because we set multiple icons in the template
+            hash: true,
+            minify: {
+                caseSensitive: true,
+                collapseWhitespace: true,
+                conservativeCollapse: true,
+                keepClosingSlash: true,
+                minifyCSS: true,
+                minifyJS: true,
+                removeComments: true,
+                removeRedundantAttributes: true,
+                removeScriptTypeAttributes: true,
+                removeStyleLinkTypeAttributes: true,
+            }
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "sass-loader"
+                ]
+            }
+        ]
+    }
+};
