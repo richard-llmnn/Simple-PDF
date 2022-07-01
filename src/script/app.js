ec600b6a15b0d481111c862a1b0f1e8783345ed2
mlaadd <9175276-mlaadd@users.noreply.gitlab.com>
2022-07-01 18:18:35 +0200

    add pdf.js

---
+++ src/script/app.js
@@ -0,0 +1,24 @@
+import * as pdfjsLib from 'pdfjs-dist/webpack';
+
+
+const input = document.createElement("input");
+input.type = "file";
+document.body.insertAdjacentElement("beforeend", input);
+input.addEventListener("change", async e => {
+    const pdfFile = e.currentTarget.files[0]
+    const pdfDoc = await pdfjsLib.getDocument(await pdfFile.arrayBuffer()).promise
+    const pdfPage = await pdfDoc.getPage(5);
+    const pdfViewport = pdfPage.getViewport({scale: 1});
+
+    console.log(pdfViewport);
+
+
+    const canvas = document.createElement("canvas");
+    var context = canvas.getContext('2d');
+    canvas.height = pdfViewport.height;
+    canvas.width = pdfViewport.width;
+
+    document.body.insertAdjacentElement("beforeend", canvas);
+
+    pdfPage.render({canvasContext: context, viewport: pdfViewport});
+});
