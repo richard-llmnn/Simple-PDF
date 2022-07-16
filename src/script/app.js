b691790c003da9b44bafce6276628f13bee34194
rullmann <69484873+richard-llmnn@users.noreply.github.com>
2022-07-16 23:25:30 +0200

    add missing functions

--- src/script/app.js
+++ src/script/app.js
@@ -2,16 +2,17 @@ import * as pdfjsLib from 'pdfjs-dist/webpack';
 import {Sortable} from "@shopify/draggable";
 import Dropzone from "dropzone";
 
+let pageCounter = 1;
+
 // make html elements draggable
 const container = document.querySelector("#drag-area");
 const drag = new Sortable(container, {
     draggable: ".card-auto-size",
 })
 
-let myDropzone = new Dropzone("#dropzone", { autoProcessQueue: false, url: "#" });
-console.log(myDropzone)
+let myDropzone = new Dropzone("#dropzone", { autoProcessQueue: false, url: "#", acceptedFiles: ".pdf" });
 myDropzone.on("addedfile", async file => {
-    myDropzone.element.classList.add("d-none");
+    //myDropzone.element.classList.add("d-none");
     const pdfFile = file
     const pdfDoc = await pdfjsLib.getDocument(await pdfFile.arrayBuffer()).promise
     for (let pageIndex = 1; pageIndex <= await pdfDoc.numPages; pageIndex++){
@@ -32,24 +33,53 @@ myDropzone.on("addedfile", async file => {
         canvas.width = pdfViewport.width;
         canvas.classList.add("card-img-top");
         
-        const card = template(pageIndex);
-        card.insertAdjacentElement("afterbegin", canvas);
+        const card = template(pageCounter);
+        card.querySelector('.card-body').insertAdjacentElement("afterbegin", canvas);
 
         container.insertAdjacentElement("beforeend", card);
     
         pdfPage.render({canvasContext: context, viewport: pdfViewport});
+        pageCounter++;
     }
     myDropzone.removeAllFiles()
 });
 
+window.removePage = function (pageID)
+{
+    const page = document.querySelector('div[data-page-id="'+pageID+'"]');
+    console.log(page)
+    if (page && confirm(`Seite ${pageID} entfernen? | Remove page ${pageID}?`) === true) {
+        page.remove()
+    }
+}
+
+window.resetPage = function () {
+    if (confirm("Zurücksetzen/Reset?")) {
+        location.reload()
+    }
+}
+
+
+
 const template = page => {
     const container = document.createElement("span");
     container.innerHTML = `
-        <div class="card card-auto-size">
-            <div class="card-body">
-                <div class="card-title bg-warning rounded-2" style="padding:4px;position:absolute;top:10px;left:10px">${page}</div>
-                <button class="btn btn-danger"><i class="gg-trash"></i></button>
+        <div class="card card-auto-size" data-page-id="${page}">
+            <div class="card-header bg-warning p-1 text-center">${page}</div>
+            <div class="card-body p-0"></div>
+            <div class="card-footer bg-danger p-0">
+                <button class="btn btn-danger w-100 m-0 p-3" onclick="removePage(${page})"><i class="gg-trash mx-auto"></i></button>
             </div>
         </div>`;
     return container.firstElementChild;
+}
+
+window.savePDF = function ()
+{
+    const pages = document.querySelector("#drag-area").querySelectorAll("div[data-page-id]");
+    const idList = [];
+    for (const page of pages) {
+        idList.push(page.dataset.pageId)
+    }
+    // TODO: create list from page ids
 }
