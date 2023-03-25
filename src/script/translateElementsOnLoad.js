a22b1373c59bbf1dc8811cee9485ab7b45ebb775
richard-llmnn <69484873+richard-llmnn@users.noreply.github.com>
2023-03-25 21:42:33 +0100

    add translations for resize feature

--- src/script/translateElementsOnLoad.js
+++ src/script/translateElementsOnLoad.js
@@ -9,6 +9,7 @@ document.getElementById("saveButton").innerText = t({
     fr: "Enregistrer",
     zh: "拯救"
 });
+document.getElementById("resizeModalClose").innerText = document.getElementById("saveButton").innerText
 
 document.getElementById("previewButton").innerText = t({
     de: "Vorschau",
@@ -17,14 +18,7 @@ document.getElementById("previewButton").innerText = t({
     fr: "Aper",
     zh: "预览"
 });
-
-document.getElementById("staticBackdropLabel").innerText = t({
-    de: "Vorschau",
-    en: "Preview",
-    ru: "Предварительный просмотр",
-    fr: "Aper",
-    zh: "预览"
-});
+document.getElementById("staticBackdropLabel").innerText = document.getElementById("previewButton").innerText
 
 document.getElementById("resetButton").innerText = t({
     de: "Zurücksetzen",
@@ -33,6 +27,7 @@ document.getElementById("resetButton").innerText = t({
     fr: "Réinitialiser",
     zh: "复位"
 });
+document.getElementById("resizeModalReset").innerText = document.getElementById("resetButton").innerText
 
 document.getElementById("scrollUpButton").innerText = t({
     de: "Hochscrollen",
@@ -48,4 +43,36 @@ document.getElementById("closeText").innerText = t({
     ru: "Закрыть",
     fr: "Fermer",
     zh: "关闭"
+});
+
+document.getElementById("staticBackdropLabel2").innerText = t({
+    de: "Größe anpassen",
+    en: "Adjust size",
+    ru: "Изменить размер",
+    fr: "Ajuster la taille",
+    zh: "调整大小"
+});
+
+document.getElementById("presetLabel").innerText = t({
+    de: "Voreinstellung",
+    en: "Preset",
+    ru: "Предустановка",
+    fr: "Préréglage",
+    zh: "预设"
+});
+
+document.getElementById("widthLabel").innerText = t({
+    de: "Breite",
+    en: "Width",
+    ru: "Широкий",
+    fr: "Largeur",
+    zh: "宽幅"
+});
+
+document.getElementById("heightLabel").innerText = t({
+    de: "Höhe",
+    en: "Height",
+    ru: "Высота",
+    fr: "Hauteur",
+    zh: "高度"
 });
